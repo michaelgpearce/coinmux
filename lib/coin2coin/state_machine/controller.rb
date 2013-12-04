f7ea553e1d85fa32b77b2d378291a9926a228c45
Michael Pearce <michaelgpearce@yahoo.com>
2013-12-04 01:21:35 -0800

    Update

--- lib/coin2coin/state_machine/controller.rb
+++ lib/coin2coin/state_machine/controller.rb
@@ -1,8 +1,15 @@
 class Coin2Coin::StateMachine::Controller
-  attr_accessor :controller_message
+  attr_accessor :controller_message, :bitcoin_amount, :minimum_participant_size
   
-  # Status: "WaitingForInputs" | "WaitingForOutputs" | "WaitingForSignatures" | "WaitingForConfirmation" | "Failed" | "Complete"
-  state_machine :state, :initial => :waiting_for_inputs do
+  # Status: "New" | "WaitingForInputs" | "WaitingForOutputs" | "WaitingForSignatures" | "WaitingForConfirmation" | "Failed" | "Complete"
+  state_machine :state, :initial => :new do
+    event :wait_for_inputs do
+      transition [:new] => :waiting_for_inputs, :if => :bitcoin_amount_and_minimum_participant_size_valid?
+    end
+
+    state :new do
+    end
+    
     state :waiting_for_inputs do
     end
     
@@ -21,45 +28,30 @@ class Coin2Coin::StateMachine::Controller
     state :complete do
     end
     
-  #   before_transition :parked => any - :parked, :do => :put_on_seatbelt
-  # 
-  #   after_transition :on => :crash, :do => :tow
-  #   after_transition :on => :repair, :do => :fix
-  #   after_transition any => :parked do |vehicle, transition|
-  #     vehicle.seatbelt_on = false
-  #   end
-  # 
-  #   after_failure :on => :ignite, :do => :log_start_failure
-  # 
-  #   event :park do
-  #     transition [:idling, :first_gear] => :parked
-  #   end
-  # 
-  #   event :shift_up do
-  #     transition :idling => :first_gear, :first_gear => :second_gear, :second_gear => :third_gear
-  #   end
-  # 
-  #   state :parked do
-  #     def speed
-  #       0
-  #     end
-  #   end
+    before_transition :new => :waiting_for_inputs, :do => :create_controller_message
   end
 
-  def initialize(params = {:amount => nil, :minimum_size => nil})
+  # Keep empty initialize method here so super call isn't forgotten if logic added!
+  def initialize
     super() # NOTE: This *must* be called, otherwise states won't get initialized
-    
-    params.assert_valid_keys(:amount, :minimum_size)
-    
-    @controller_message = Coin2Coin::Message::Controller.new(params)
-    insert_key = Coin2Coin::Config.instance['coin_joins'][params[:amount].to_s]['insert_key']
-    
-    Coin2Coin::Freenet.instance.insert(insert_key, @controller_message.to_json) do |event|
+  end
+  
+  private
+  
+  def create_controller_message
+    self.controller_message = Coin2Coin::Message::Controller.new(:amount => bitcoin_amount, :minimum_size => minimum_participant_size)
+    insert_key = Coin2Coin::Config.instance['coin_joins'][bitcoin_amount.to_s]['insert_key']
+  
+    Coin2Coin::Freenet.instance.insert(insert_key, controller_message.to_json) do |event|
       if event.error.nil?
-        control_status_message = Coin2Coin::Message::ControlStatus.new(:status => state)
-        Coin2Coin::Freenet.instance.insert(@controller_message.control_status_queue.read_only_insert_key, control_status_message.to_json) do |event|
+        control_status_message = Coin2Coin::Message::ControlStatus.new(:status => 'WaitingForInputs')
+        Coin2Coin::Freenet.instance.insert(controller_message.control_status_queue.read_only_insert_key, control_status_message.to_json) do |event|
         end
       end
     end
+  end
+  
+  def bitcoin_amount_and_minimum_participant_size_valid?
+    bitcoin_amount > 0 && minimum_participant_size.to_i >= 1
   end
 end
--- spec/state_machine/controller_spec.rb
+++ spec/state_machine/controller_spec.rb
@@ -6,30 +6,45 @@ describe Coin2Coin::StateMachine::Controller do
   end
   
   context "#initialize" do
-    let(:amount) { 100_000_000 }
-    let(:minimum_size) { 5 }
-    let(:coin_join_request_key) { Coin2Coin::Config.instance['coin_joins'][amount.to_s]['request_key'] }
+    subject do
+      Coin2Coin::StateMachine::Controller.new
+    end
+    
+    it "should be in the new state" do
+      expect(subject.state).to eq('new')
+    end
+  end
+  
+  context "#wait_for_inputs" do
+    let(:controller) { Coin2Coin::StateMachine::Controller.new }
+    let(:bitcoin_amount) { 100_000_000 }
+    let(:minimum_participant_size) { 5 }
+    let(:coin_join_request_key) { Coin2Coin::Config.instance['coin_joins'][bitcoin_amount.to_s]['request_key'] }
     
     subject do
-      Coin2Coin::StateMachine::Controller.new(:amount => amount, :minimum_size => minimum_size)
+      controller.minimum_participant_size = minimum_participant_size
+      controller.bitcoin_amount = bitcoin_amount
+      controller.wait_for_inputs
     end
     
-    it "inserts controller message" do
-      controller = subject
+    context "with valid input" do
+      it "has status waiting_for_inputs" do
+        subject
+        
+        expect(controller.state).to eq('waiting_for_inputs')
+      end
       
-      expect(controller.controller_message.to_json).to eq(fake_freenet.fetch(coin_join_request_key).last)
-    end
-    
-    it "inserts controller message" do
-      controller = subject
-      
-      expect(controller.controller_message.to_json).to eq(fake_freenet.fetch(coin_join_request_key).last)
-    end
-    
-    it "inserts control status message" do
-      controller = subject
-      
-      expect(Coin2Coin::Message::ControlStatus.new(:status => 'waiting_for_inputs', :transaction_id => nil).to_json).to eq(fake_freenet.fetch(controller.controller_message.control_status_queue.request_key).last)
+      it "inserts controller message" do
+        subject
+
+        expect(fake_freenet.fetch(coin_join_request_key).last).to eq(controller.controller_message.to_json)
+      end
+
+      it "inserts control status message" do
+        subject
+
+        expect(fake_freenet.fetch(controller.controller_message.control_status_queue.request_key).last).to eq(Coin2Coin::Message::ControlStatus.new(:status => 'WaitingForInputs', :transaction_id => nil).to_json)
+      end
     end
   end
 end
