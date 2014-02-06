818acd680e65856085942be486ed8114373e3284
Michael Pearce <michaelgpearce@yahoo.com>
2014-02-05 23:06:36 -0800

    Use temp dir when cannot mkdir at Coinmux.root

--- lib/coinmux/file_util.rb
+++ lib/coinmux/file_util.rb
@@ -1,12 +1,13 @@
 require 'fileutils'
+require 'tmpdir'
 
 module Coinmux::FileUtil
   class << self
-    def root_mkdir_p(*path)
+    def root_mkdir_p(*paths)
       begin
-        FileUtils.mkdir_p(path = File.join(Coinmux.root, *path))
+        FileUtils.mkdir_p(path = File.join(Coinmux.root, *paths))
       rescue
-        FileUtils.mkdir_p(path = File.join('.', *path))
+        FileUtils.mkdir_p(path = File.join(Dir.tmpdir, *paths))
       end
 
       path
