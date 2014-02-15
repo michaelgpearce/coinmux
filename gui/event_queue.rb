7ed5498918f6cbb334fa2bd65ca7047f75b157d5
Michael Pearce <michaelgpearce@yahoo.com>
2014-02-14 22:56:58 -0800

    Show errors in thread to stdout.

--- gui/event_queue.rb
+++ gui/event_queue.rb
@@ -8,19 +8,31 @@ class Gui::EventQueue
     if SwingUtilities.isEventDispatchThread()
       yield
     else
-      SwingUtilities.invokeAndWait(&callback)
+      SwingUtilities.invokeAndWait(create_callback_with_error_handling(&callback))
     end
   end
   
   def future_exec(seconds = 0, &callback)
     if seconds <= 0
-      SwingUtilities.invokeLater(&callback)
+      SwingUtilities.invokeLater(create_callback_with_error_handling(&callback))
     else
       Timer.new(seconds * 1000, lambda { |e|
-        yield
+        create_callback_with_error_handling(&callback).call
       }).tap do |timer|
         timer.setRepeats(false)
         timer.start()
+      end
+    end
+  end
+
+  private
+
+  def create_callback_with_error_handling(&callback)
+    Proc.new do
+      begin
+        yield
+      rescue Exception => e
+        puts "Unhandled error in event dispatch thread: #{e}", *e.backtrace
       end
     end
   end
