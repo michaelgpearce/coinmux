9cf7641a1e9c1507b1e923bf58b4c77b8809f601
Michael Pearce <michaelgpearce@yahoo.com>
2014-02-16 03:08:28 -0800

    Ability to call wait_for_callback as a class method.

--- lib/coinmux/threading.rb
+++ lib/coinmux/threading.rb
@@ -1,8 +1,8 @@
 module Coinmux::Threading
-  def wait_for_callback(*args, &callback)
+  def self.wait_for_callback(object, method, *args, &callback)
     done = false
     results = nil
-    send(*args) do |*callback_results|
+    object.send(method, *args) do |*callback_results|
       done = true
       results = callback_results
     end
@@ -10,5 +10,9 @@ module Coinmux::Threading
     loop { break if done; sleep(0.05) }
 
     results
+  end
+
+  def wait_for_callback(method, *args, &callback)
+    Coinmux::Threading.wait_for_callback(self, method, *args, &callback)
   end
 end
